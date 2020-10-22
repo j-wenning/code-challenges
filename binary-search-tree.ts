830fe0eebb6755fe56e4a5e8dd8f68d40a4bb7d4
j-wenning <wenning.justin@gmail.com>
2020-10-22 13:35:01 -0700

    Updated binary search tree script remove method to account for cases where nodes dont exist Removed bst find overloads Added an extra preemptive pass through balance (not entirely sure why this works) Made minor syntax improvements in other methods Updated tests to use new test log Tidied tree test values

--- binary-search-tree.ts
+++ binary-search-tree.ts
@@ -10,21 +10,22 @@ class BSTNode {
 }
 
 class BinarySearchTree {
-    private root: BSTNode
+    root: BSTNode
     private isNode (item: number, curNode: BSTNode): boolean
     private isNode (item: BSTNode, curNode: BSTNode): boolean
     private isNode (item: any, curNode: BSTNode) {
-        if (typeof item === typeof Number()) return item === curNode.val
-        return item === curNode
+        return typeof item === typeof Number()
+            ? item === curNode.val
+            : item === curNode
     }
     private isNodeParent (item: number, curNode: BSTNode): boolean
     private isNodeParent (item: BSTNode, curNode: BSTNode): boolean
     private isNodeParent (item: any, curNode: BSTNode) {
-        if (typeof item === typeof Number()) {
-            return curNode.l?.val === item || curNode.r?.val === item
-        }
-        return curNode.l === item || curNode.r === item
+        return typeof item === typeof Number()
+            ? item === curNode.l?.val || item === curNode.r?.val
+            : item === curNode.l || item === curNode.r
     }
+    private calcDepthDiff (node: BSTNode): number { return this.maxDepth(node.r) - this.maxDepth(node.l)}
     constructor (vals: Set<number> = new Set()) { vals.forEach(val => this.add(val)) }
     add (item: number, curNode?: BSTNode): BSTNode
     add (item: BSTNode, curNode?: BSTNode): BSTNode
@@ -32,7 +33,6 @@ class BinarySearchTree {
         const itemNode = typeof item === typeof Number()
             ? new BSTNode(item)
             : item
-        if (!itemNode) return null
         if (!curNode) {
             if (!this.root) this.root = itemNode
             return itemNode
@@ -44,20 +44,19 @@ class BinarySearchTree {
     remove (item: number, curNode?: BSTNode): BSTNode
     remove (item: BSTNode, curNode?: BSTNode): BSTNode
     remove (item: any, curNode: BSTNode = this.root) {
-        const foundParent = this.find(item, this.isNodeParent, curNode)
-        const foundNode = this.find(item, this.isNode, foundParent)
-        foundParent[foundParent.l === foundNode ? 'l' : 'r'] = foundNode.l || foundNode.r
+        const foundParent: BSTNode = this.find(item, this.isNodeParent, curNode)
+        const foundNode: BSTNode = this.find(item, this.isNode, foundParent)
+        if (!foundNode) return undefined
+        if (foundNode === this.root) this.root = foundNode.l || foundNode.r
+        else foundParent[foundParent.l === foundNode ? 'l' : 'r'] = foundNode.l || foundNode.r
         if (foundNode.l && foundNode.r) this.add(foundNode.r, foundNode.l)
         foundNode.l = foundNode.r = null
         return foundNode
     }
-    find (item: number, query?: Function, curNode?: BSTNode): BSTNode
-    find (item: BSTNode, query?: Function, curNode?: BSTNode): BSTNode
-    find (item: any, query: Function = this.isNode, curNode: BSTNode = this.root) {
-        const itemVal = typeof item === typeof Number() ? item : item.val
+    find (item: number, query: Function = this.isNode, curNode: BSTNode = this.root): BSTNode {
         if (!curNode || query(item, curNode)) return curNode
-        if (itemVal > curNode.val) return this.find(itemVal, query, curNode.r)
-        if (itemVal < curNode.val) return this.find(itemVal, query, curNode.l)
+        if (item > curNode.val) return this.find(item, query, curNode.r)
+        if (item < curNode.val) return this.find(item, query, curNode.l)
     }
     maxDepth (curNode: BSTNode = this.root): number {
         if (!curNode) return 0
@@ -67,22 +66,25 @@ class BinarySearchTree {
         if (!curNode) return 0
         return 1 + this.count(curNode.l) + this.count(curNode.r)
     }
-    rotateNodes (base: BSTNode, newBase: BSTNode) {
-        base[base.r === newBase ? 'r' : 'l'] = null
+    rotateNodes (base: BSTNode, newBase: BSTNode): BSTNode {
+        if (base.l === newBase) base.l = null
+        else if (base.r === newBase) base.r = null
+        else return base
         if (base === this.root) {
             this.root = newBase
         } else {
             const parent = this.find(base.val, this.isNodeParent)
-            parent[base.val > parent.val ? 'r' : 'l'] = newBase
+            parent[parent.l === base ? 'l' : 'r'] = newBase
         }
         this.add(base, newBase)
         return newBase
     }
     balance (curNode: BSTNode = this.root): void {
-        if (!curNode) return
-        const calcDepthDiff = () => this.maxDepth(curNode.r) - this.maxDepth(curNode.l)
+        if (!curNode) return undefined
         let depthDiff
-        while (Math.abs((depthDiff = calcDepthDiff())) > 1) {
+        this.balance(curNode.l)
+        this.balance(curNode.r)
+        while (Math.abs((depthDiff = this.calcDepthDiff(curNode))) > 1) {
             curNode = this.rotateNodes(curNode, depthDiff > 0 ? curNode.r : curNode.l)
         }
         this.balance(curNode.l)
@@ -92,14 +94,35 @@ class BinarySearchTree {
 
 module.exports = (tests: boolean = false): void => {
     if (tests) {
-        const treeTest = new BinarySearchTree (new Set<number> ([342,3415,1,2,3,4,5,6,7,8,9,10,11,12,13,987564,78546123,78654416131854646531,14,15,16,17,18,19,20,3246643,3246,326324,34,5325,-1,-10,-324,-46456,132,-23546236,345345,3245,342,53,45,43,5,767,5,65,8,678,567,43,341,789,46548798,8798754654321,7897,46546546546541,123546,46589741]))
-        console.log(treeTest.count())
-        treeTest.remove(14)
-        treeTest.remove(-10)
-        console.log(treeTest.count())
-        console.log('max depth:', treeTest.maxDepth())
-        treeTest.balance()
-        console.log('max depth:', treeTest.maxDepth())
-        console.log (treeTest.find(123) ?? '', '\n', treeTest.find(342), '\n', treeTest.find(3246643), '\n', treeTest.find(8), '\n', treeTest.find(78546123))
+        const testLog = require('./test-log')
+        const treeTest = new BinarySearchTree (new Set<number> ([
+            342,3415,1,2,3,4,5,6,7,8,9,10,11,12,13,987564,78546123,
+            78654416131854646531,14,15,16,17,18,19,20,3246643,3246,
+            326324,34,5325,-1,-10,-324,-46456,132,-23546236,345345,
+            3245,342,53,45,43,5,767,5,65,8,678,567,43,341,789,7897,
+            46548798,8798754654321,46546546546541,123546,465897418,
+            78794564231541,654135241,513464,124716541,156489,31,48,
+        ]))
+        console.log('\n\nBINARY SEARCH TREE TESTS\n')
+        testLog(
+            () => treeTest.count(),
+            () => treeTest.find(123),
+            () => treeTest.find(342),
+            () => treeTest.find(78546123),
+            () => treeTest.find(14),
+            () => treeTest.find(-10),
+            () => treeTest.remove(123),
+            () => treeTest.remove(14),
+            () => treeTest.remove(-10),
+            () => treeTest.find(14),
+            () => treeTest.find(-10),
+            () => treeTest.count(),
+            () => treeTest.maxDepth(),
+            () => treeTest.balance(),
+            () => treeTest.find(342),
+            () => treeTest.find(78546123),
+            () => treeTest.count(),
+            () => treeTest.maxDepth(),
+        )
     }
 }
